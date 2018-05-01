55585cab3e6a676032cb142434af973b75c1fdfc
Justin Rubek <rubejus@bvu.edu>
2018-04-30 19:30:16 -0500

    PostsView now properly 404s

--- src/PostsView.jsx
+++ src/PostsView.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import card_styles from "./css/card.css";
 import style from "./css/postsview.css";
 import { get, try_timeout } from "./utility";
